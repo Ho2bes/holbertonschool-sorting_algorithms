25a8cca8297fd925856f13df1fda4e8e724fb8e7
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-04-10 13:22:37 +0000

    the edit by vhat

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,40 +1,42 @@
 #include "sort.h"
-
 /**
  * swap - Swaps two elements of an array
- * @a: Integer a
- * @b: Integer b
+ * @a: Pointer to the first element
+ * @b: Pointer to the second element
  */
+
 void swap(int *a, int *b)
 {
-	int temp = *a;
-	*a = *b;
-	*b = temp;
+int temp = *a;
+*a = *b;
+*b = temp;
 }
 
 /**
- * bubble_sort - Doubly linked list node
- * @array: Array of integers to print
+ * bubble_sort - Sorts an array of integers
+ * @array: Array of integers to sort
  * @size: Size of the array
  */
+
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
-
-	if (size < 2)
-		return;
-
-	for (i = 0; i < size - 1; i++)
-	{
-		/*Last i elements are already in place*/
-		for (j = 0; j < size - i - 1; j++)
-		{
-			/*Swap if the element found is greater than the next element*/
-			if (array[j] > array[j + 1])
-			{
-				swap(&array[j], &array[j + 1]);
-				print_array(array, size);
-			}
-		}
-	}
+size_t i, j;
+int swapped;
+if (size < 2)
+return;
+for (i = 0; i < size - 1; i++)
+{
+swapped = 0;
+for (j = 0; j < size - i - 1; j++)
+{
+if (array[j] > array[j + 1])
+{
+swap(&array[j], &array[j + 1]);
+print_array(array, size);
+swapped = 1;
+}
+}
+if (!swapped)
+break;
+}
 }
