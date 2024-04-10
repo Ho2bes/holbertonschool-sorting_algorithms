7dd8519ba1e855d5e4f981ad71f2eca9058e8d62
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-04-10 09:58:04 +0000

    tarek code

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,34 +7,34 @@
  */
 void swap(int *a, int *b)
 {
-  int temp = *a;
-  *a = *b;
-  *b = temp;
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 }
 
 /**
- * bubble_sort - Sorts an array of integers in ascending order
- * @array: Array of integers to sort
+ * bubble_sort - Doubly linked list node
+ * @array: Array of integers to print
  * @size: Size of the array
- *
- * Description: Implements the Bubble Sort algorithm to sort the array.
- * Prints the array after each iteration.
  */
 void bubble_sort(int *array, size_t size)
 {
-  size_t i, j;
-  if (size < 2)
-    return;
-  for (i = 0; i < size - 1; i++)
-    {
-      for (j = 0; j < size - i - 1; j++)
+	size_t i, j;
+
+	if (size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
 	{
-	  /* Exchange if the current element is greater than the next element */
-	  if (array[j] > array[j + 1])
-	    {
-	      swap(&array[j], &array[j + 1]);
-	      print_array(array, size);
-	    }
+		/*Last i elements are already in place*/
+		for (j = 0; j < size - i - 1; j++)
+		{
+			/*Swap if the element found is greater than the next element*/
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				print_array(array, size);
+			}
+		}
 	}
-    }
 }
