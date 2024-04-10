b318ce31533bbef038429918aeb6eee1c3b661bb
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-04-10 08:38:51 +0000

    try the fix task zero

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,41 +1,40 @@
 #include "sort.h"
 
 /**
- * bubble_sort - Sorts an array of integers in ascending order using
- *		the Bubble Sort algorithm.
- *
- * @array: Pointer to the array to be sorted.
- * @size: Size of the array.
+ * swap - Swaps two elements of an array
+ * @a: Integer a
+ * @b: Integer b
  */
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
 
+/**
+ * bubble_sort - Doubly linked list node
+ * @array: Array of integers to print
+ * @size: Size of the array
+ */
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
-	int temp;
-	int swapped;
 
-	if (array == NULL || size < 2)
+	if (size < 2)
 		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		swapped = 0;
-
+		/*Last i elements are already in place*/
 		for (j = 0; j < size - i - 1; j++)
 		{
+			/*Swap if the element found is greater than the next element*/
 			if (array[j] > array[j + 1])
 			{
-				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
-
-				swapped = 1;
-
+				swap(&array[j], &array[j + 1]);
 				print_array(array, size);
 			}
 		}
-
-		if (swapped == 0)
-			break;
 	}
 }
